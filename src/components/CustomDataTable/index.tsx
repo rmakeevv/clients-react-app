dea28cbcae04f3cbe6e9b740ca170f62fd768f26
rmakee <r.makeevv@gmail.com>
2024-01-19 19:47:48 +0400

    update

--- src/components/CustomDataTable/index.tsx
+++ src/components/CustomDataTable/index.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import data from 'service/clients.json';
+import styles from './index.module.css';
+
+interface ClientsIndex {
+  [key: string]: any;
+}
+
+interface IClients extends ClientsIndex {
+  id: number;
+  fullname: string;
+  created_at: string;
+  phone: string;
+  region: string;
+  status: string;
+}
 
 console.log(data);
 
 const CustomDataTable = () => {
-  return (
-    <div>
-      {data.map((item) => (
-        <div key={item.id}>{item.fullname}</div>
-      ))}
-    </div>
-  );
+  const [tableData, setTableData] = useState<IClients[] | undefined>(undefined);
+
+  useEffect(() => {
+    setTableData(data);
+  }, []);
+
+  if (tableData)
+    return (
+      <div className={styles.table__row}>
+        {tableData.map((item) => {
+          for (const key in item) {
+            return <div key={item.id}>{item[key]}</div>;
+          }
+        })}
+      </div>
+    );
+
+  return <div>Loading...</div>;
 };
 
 export default CustomDataTable;
