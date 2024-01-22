2b3262688c1b5a7ff52c45b2500f59cf5c6c98e3
rmakee <r.makeevv@gmail.com>
2024-01-23 02:08:28 +0400

    update

--- src/components/CustomDataTable/index.tsx
+++ src/components/CustomDataTable/index.tsx
@@ -13,53 +13,53 @@ const CustomDataTable = () => {
 
   if (clients)
     return (
-      <table>
-        <thead>
-          <tr className={styles.filter__panel}>
-            <th>
-              <TextInput
-                value={searchInputValue}
-                onChange={(event) => setSearchInputValue(event.target.value)}
-              />
-            </th>
-          </tr>
+      <div className={styles.table__container}>
+        <div className={styles.filter__panel}>
+          <TextInput
+            value={searchInputValue}
+            onChange={(event) => setSearchInputValue(event.target.value)}
+          />
+        </div>
 
-          <tr>
-            <th>id</th>
-            <th>ФИО</th>
-            <th>Дата создания</th>
-            <th>Телефон</th>
-            <th>Регион</th>
-            <th>Статус</th>
-            <th></th>
-          </tr>
-        </thead>
-
-        <tbody className={styles.table__body}>
-          {clients.map((item) => (
-            <tr key={item.id} className={styles.table__row}>
-              <td className={styles.table__cell}>{item.id}</td>
-              <td className={styles.table__cell}>
-                <Link to={`clients/${item.id}`}>{item.fullname}</Link>{' '}
-              </td>
-              <td className={styles.table__cell}>
-                {new Date(item.created_at).toLocaleDateString('ru')}
-              </td>
-              <td className={styles.table__cell}>{item.phone}</td>
-              <td className={styles.table__cell}>{item.region}</td>
-              <td className={styles.table__cell}>{item.status}</td>
-              <td className={styles.table__cell}>
-                <button
-                  className={styles.delete__button}
-                  onClick={() => dispatch(deleteOne(item.id))}
-                >
-                  <TrashIcon />
-                </button>
-              </td>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ width: '20px' }}>id</th>
+              <th>ФИО</th>
+              <th>Дата создания</th>
+              <th>Телефон</th>
+              <th>Регион</th>
+              <th>Статус</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody className={styles.table__body}>
+            {clients.map((item) => (
+              <tr key={item.id} className={styles.table__row}>
+                <td className={styles.table__cell}>{item.id}</td>
+                <td className={styles.table__cell}>
+                  <Link to={`clients/${item.id}`}>{item.fullname}</Link>{' '}
+                </td>
+                <td className={styles.table__cell}>
+                  {new Date(item.created_at).toLocaleDateString('ru')}
+                </td>
+                <td className={styles.table__cell}>{item.phone}</td>
+                <td className={styles.table__cell}>{item.region}</td>
+                <td className={styles.table__cell}>{item.status}</td>
+                <td className={styles.table__cell}>
+                  <button
+                    className={styles.delete__button}
+                    onClick={() => dispatch(deleteOne(item.id))}
+                  >
+                    <TrashIcon />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 
   return <div>Loading...</div>;
